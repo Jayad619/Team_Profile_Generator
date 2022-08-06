2e133463bd7eccf6f45449ec6d1017f5b4ea0881
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-08-06 22:41:47 +0100

    Added script + tested and read template file

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const inquirer = require('inquirer');
+const { promises: fs } = require('fs')
 
 async function managerQuestions() {
-    console.log('WORKING')
+    const answers = await inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name manager?'
+    }, {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your ID number please'
+    }, {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address please'
+    }, {
+        type: 'input',
+        name: 'office number',
+        message: 'What is your office number please'
+    }])
+
+    const templateContent = await fs.readFile('./index.html');
+    console.log((templateContent).toString())
 }
 
 managerQuestions()
