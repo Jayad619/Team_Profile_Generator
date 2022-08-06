a74d2277d4ff9985afc251d47247f2dc04169db8
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-08-06 22:37:34 +0100

    TEST

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const inquirer = require("inquirer");
-const fs = require("fs"); // do I need this?
-const path = require("path"); // do I need this?
+const inquirer = require('inquirer');
 
-const employee = require(".lib/employee.js"); // this might not be needed - pass through manager/engineer/intern?
-const manager = require(".lib/manager.js");
-const engineer = require(".lib/engineer.js");
-const intern = require(".lib/intern.js")
+async function managerQuestions() {
+    console.log('WORKING')
+}
 
-// inquirer - first questions (manager)
-// then, create object with responses
-// then, either run for engineer, intern, or end
-// engineer questions
-// intern questions
-// run for engineer, intern, or end (repeated code)
-// end and build web page.
+managerQuestions()
